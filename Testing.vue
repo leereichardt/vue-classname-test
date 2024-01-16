659e88d289da791cd17a9e0a10d9f5a159a0e156
Lee Reichardt <lee.reichardt@rocketspark.com>
2024-01-16 15:38:32 +1300

    Testing

---
+++ Testing.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {useCssModule} from "vue";
+const $style = useCssModule();
+</script>
+
+<template>
+  <div :class="$style.container"></div>
+</template>
+
+<style module>
+.container {
+
+}
+</style>
